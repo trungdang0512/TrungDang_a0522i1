7010e58ef5917b5c50f904047009a0b11bb0ccf1
trungdang0512 <vantrung0512@gmail.com>
2023-03-23 23:15:36 +0700

    23.03

--- Module 05/CaseStudy/furama-cs/src/app/component/service-managerment/service-edit/service-edit.component.ts
+++ Module 05/CaseStudy/furama-cs/src/app/component/service-managerment/service-edit/service-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Service} from "../../../model/service";
 
 @Component({
   selector: 'app-service-edit',
@@ -7,11 +8,29 @@ import {FormGroup} from "@angular/forms";
   styleUrls: ['./service-edit.component.css']
 })
 export class ServiceEditComponent implements OnInit {
+  @Input()
+  service: Service = {};
+
   editServiceForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.editServiceForm = new FormGroup({
+      id: new FormControl(),
+      nameService: new FormControl(),
+      area: new FormControl(),
+      floor: new FormControl(),
+      peopleNumber: new FormControl(),
+      cost: new FormControl(),
+      hireType: new FormControl(),
+      status: new FormControl()
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges):void{
+    console.log(changes);
   }
 
   editService() {
